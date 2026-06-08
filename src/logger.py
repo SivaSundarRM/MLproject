2e6da006e6c5a886bcc98b15c1aa515031d8554b
SivaSundarRM <sivaramachandran143@gmail.com>
2026-06-08 12:00:33 +0530

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -1,23 +1,17 @@
 import logging
 import os
-import datetime
+from datetime import datetime
 
-LOG_FILE = f"logs/{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs",LOG_FILE)
-os.makedirs(os.path.dirname(logs_path), exist_ok=True)
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(os.getcwd(), "logs", LOG_FILE)
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
+    force=True
 )
-
-
-if __name__ == "__main__":
-    logging.info("This is an info message.")
-    logging.warning("This is a warning message.")
-    logging.error("This is an error message.")
